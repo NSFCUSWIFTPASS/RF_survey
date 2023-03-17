16d73abddc1910a1d06494a2283c60fed2a9482b
Stefan Tschimben <stefan.tschimben@colorado.edu>
2023-03-17 08:43:03 -0600

    Update Watcher.sh

--- server/Watcher.sh
+++ server/Watcher.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# (c) 2022 The Regents of the University of Colorado, a body corporate. Created by Stefan Tschimben.
-# This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 
-# To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
-
 source $HOME/.profile
 
 ScriptName="$(basename $0)"
