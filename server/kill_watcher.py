f5530aa916d12d9fc680e3608df2fd211273cd7e
NSFCUSWIFTPASS <126233437+NSFCUSWIFTPASS@users.noreply.github.com>
2024-03-08 11:23:28 -0700

    Delete server directory

--- server/kill_watcher.py
+++
@@ -1,9 +0,0 @@
-# Simple code executed to gracefully kill a running script.
-# It reads the PID created by Watcher.py and sends the termination signal to the PID
-
-import os
-from signal import SIGTERM
-
-with open(os.environ["HOME"]+"/watcher.pid", "r") as f:
-    pid = int(f.readline())
-os.kill(pid, SIGTERM)
