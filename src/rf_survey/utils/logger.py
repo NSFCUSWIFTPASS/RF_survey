ea4f550552b00e628a636ffa1b21f1434b705f48
0xeb-bp <53204152+0xeb-bp@users.noreply.github.com>
2025-10-14 12:57:58 -0600

    Slight refactor and bug fix.

--- src/rf_survey/utils/logger.py
+++ src/rf_survey/utils/logger.py
@@ -3,45 +3,61 @@
 # To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 
 import os
+import sys
 import logging
-from datetime import time
+from datetime import datetime
 from logging.handlers import TimedRotatingFileHandler
-from syslog import syslog
 
 
-class Logger(object):
-    def __init__(self, key, path, log_file):
-        self.key = key
-        self.path = path
-        self.log_file = log_file
-        if not os.path.exists(self.path):
-            try:
-                os.makedirs(self.path)
-            except:
-                print("Error when creating directory")
+class Logger:
+    def __init__(self, name: str, log_path: str = "~/logs", log_level: str = "INFO"):
+        self.name = name.lower()
 
-        for handler in logging.root.handlers[:]:
-            logging.root.removeHandler(handler)
+        self.log_path = os.path.expanduser(log_path)
 
-        # create logger
-        self.logger = logging.getLogger(str(self.key).lower())
+        log_time = datetime.now().strftime("%Y-%m-%d")
+        self.log_file = f"{self.name}-{log_time}.log"
+
+        self.logger = logging.getLogger(self.name)
         self.logger.setLevel(logging.DEBUG)
 
-        if not self.logger.handlers:
-            self.logger.propagate = 0
-            handler = TimedRotatingFileHandler(
-                self.path + self.log_file, when="D", interval=1, atTime=time(0, 0, 1)
+        # check if handlers are already present
+        if self.logger.hasHandlers():
+            return
+
+        self.logger.propagate = False
+
+        try:
+            os.makedirs(self.log_path, exist_ok=True)
+
+            # file handler
+            file_handler = TimedRotatingFileHandler(
+                os.path.join(self.log_path, self.log_file),
+                when="midnight",
+                interval=1,
+                backupCount=7,
+                utc=True,
             )
-            handler.suffix = "%Y%m%d"
+
+            file_handler.setLevel(log_level.upper())
             formatter = logging.Formatter(
                 "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
             )
-            handler.setFormatter(formatter)
-            self.logger.handlers = []
-            self.logger.addHandler(handler)
+            file_handler.setFormatter(formatter)
+            self.logger.addHandler(file_handler)
+
+            console_handler = logging.StreamHandler(sys.stderr)
+            console_handler.setLevel(logging.WARNING)
+            console_handler.setFormatter(formatter)
+            self.logger.addHandler(console_handler)
+
+        except (OSError, IOError) as e:
+            print(
+                f"CRITICAL: Could not configure logger '{self.name}'. Error: {e}",
+                file=sys.stderr,
+            )  # fall back to stderr
 
     def write_log(self, level, message):
-        # writes to a local log as well as syslog
         if level == "INFO":
             self.logger.info(message)
         if level == "DEBUG":
@@ -52,5 +68,3 @@ class Logger(object):
             self.logger.error(message)
         if level == "CRITICAL":
             self.logger.critical(message)
-        syslog(message)
-
